366134e32a96d39a1c3cb28ae22af30f25a10a94
SkyHuang0715 <she515996344@gmail.com>
2019-05-17 17:12:03 -0400

    merge scene

---
+++ CodeLab1-2019-Final/Assets/SphereCastpickup.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereCastpickup : MonoBehaviour
+{
+    
+
+    private Transform pickedUpObject;
+
+    //private Vector3 lastPosition;
+
+    //private List<Transform> objToMove = new List<Transform>();
+    
+
+    public LayerMask mask;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            Collider[] pickobjs;
+            pickobjs = Physics.OverlapSphere(transform.position + Vector3.up, 2.0f, mask);
+            
+            //objToMove = new List<Transform>();
+            foreach (Collider c in pickobjs)
+            {
+                if (c.gameObject.name == "Cube")
+                {
+                    Destroy(c.gameObject,2);
+                }
+                
+                //objToMove.Add(c.transform);
+            }
+        }
+
+       // foreach (Transform t in objToMove)
+        {
+            //t.position += transform.position - lastPosition;
+            
+        }//Position = transform.position;
+        
+    }
+}

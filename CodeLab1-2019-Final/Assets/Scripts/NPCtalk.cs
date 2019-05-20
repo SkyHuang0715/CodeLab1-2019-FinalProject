3fb6272ba90fef9d2d4eb50dd1ac1d20deeb07ea
SkyHuang0715 <she515996344@gmail.com>
2019-05-20 04:48:07 -0400

    merge AI npc

---
+++ CodeLab1-2019-Final/Assets/Scripts/NPCtalk.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCtalk : MonoBehaviour
+{
+    //定义NPC对话数据
+    private string[] mData =
+    {
+        "你好,我是NPC", "欢迎来到幻想世界",
+        "可以请你帮我去打怪物吗？", "我会给你一定的奖励", "请打死守在怪物洞口前的侍卫兵", "我旁边的南瓜可以增加你的生命值"
+    };
+
+//当前对话索引
+    private int index = 0;
+
+//用于显示对话的GUI Text
+    public GUIText NPCText;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //从角色位置向NPC发射一条经过鼠标位置的射线
+        Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit mHi;
+        //判断是否击中了NPC
+        if (Physics.Raycast(mRay, out mHi, 10))
+        {
+            if (mHi.collider.gameObject.tag == "NPC")
+
+               // 点击鼠标， 就会进行对话
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+            //绘制指定索引的对话文本
+                if (index < mData.Length)
+                {
+                    NPCText.text = "NPC:" + mData[index];
+                    index = index + 1;
+                }
+                else
+                {
+                    NPCText.enabled = false; //不显示
+//index=0;
+                }
+            }
+        }
+    }
+}
--- CodeLab1-2019-Final/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
+++ CodeLab1-2019-Final/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -1,5 +1,8 @@
 using System;
 using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+
 
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
@@ -11,7 +14,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         public ThirdPersonCharacter character { get; private set; } // the character we are controlling
         public Transform target;                                    // target to aim for
 
-
+        public bool playertouch;
         private void Start()
         {
             // get the components on the object we need ( should not be null due to require component so no need to check )
@@ -20,24 +23,63 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 	        agent.updateRotation = false;
 	        agent.updatePosition = true;
+	        
+            InvokeRepeating("targetChange",0, 20);
         }
 
 
         private void Update()
         {
             if (target != null)
-                agent.SetDestination(target.position);
+                agent.SetDestination(target.transform.position);
 
-            if (agent.remainingDistance > agent.stoppingDistance)
-                character.Move(agent.desiredVelocity, false, false);
-            else
+            if (agent.remainingDistance > agent.stoppingDistance && playertouch == false)
+            { character.Move(agent.desiredVelocity, false, false);}
+           
+            else if (agent.remainingDistance <= agent.stoppingDistance && playertouch == false)
+            {
                 character.Move(Vector3.zero, false, false);
+                
+            }
+            else if (playertouch == true)
+            {
+                character.Move(Vector3.zero, false, false);
+            }
+
+            
+            Debug.Log(playertouch);
         }
 
 
         public void SetTarget(Transform target)
         {
             this.target = target;
+        }
+
+        void targetChange()
+        {
+            
+            //yield return new WaitForSeconds(5);
+          
+            target.transform.position = new Vector3(Random.Range(80,118),30,Random.Range(94,97));
+           // characterstop = false;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+           
+            if (other.CompareTag("Player")) //if player attached npc
+            {
+                playertouch = true;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player")) //if player attached npc
+            {
+                playertouch = false;
+            }
         }
     }
 }
